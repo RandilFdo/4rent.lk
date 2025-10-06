46fa7637459f53a88453d1db4164578f5b1a7844
Randil Fernando <randilfernando829@gmail.com>
2025-10-06 12:36:21 +0530

    Add OAuth test page for debugging Vercel OAuth issues

---
+++ .history/app/components/Container_20251006123429.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+interface ContainerProps {
+   children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+   return <div className="max-w-[2520px] mx-auto xl:px-16 md:px-12 sm:px-8 px-4">{children}</div>;
+};
+export default Container;
